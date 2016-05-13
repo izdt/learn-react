6fbdd49dfcfb912954bc06533a2d9d10dd7c64d3
ZDT@MAC <dev@dthappy.com>
2016-05-14 00:52:02 +0800

    add material-ui

--- chatApp/src/components/App.jsx
+++ chatApp/src/components/App.jsx
@@ -1,26 +1,37 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import {cyan500} from 'material-ui/styles/colors';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: cyan500,
+  },
+  appBar: {
+    height: 50,
+  },
+});
 
 class App extends Component {
     constructor(){
         super();
-        this.state = {
-            messages:[
-                'hi there how are you?',
-                'I am fine, and you?'
-            ]
-        }
+        
     }
+        
     render() {
-        //debugger;
-        var messageNode = this.state.messages.map((message)=>{
-           return (
-            <div>{message}</div>     
-           );
-        });
-        //console.log(messageNode);
-        return ( 
+        return (
             <div>
-                 {messageNode}
+                <MuiThemeProvider muiTheme={muiTheme}>
+                    <AppBar
+                        title="Title"
+                        iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    />
+                </MuiThemeProvider>
+                <MessageList />
             </div>
         );
     }
---
+++ chatApp/src/components/Message.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                {this.props.message}
+            </div>
+        );
+    }
+}
+
+export default Message;
