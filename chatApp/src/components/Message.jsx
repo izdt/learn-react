349f0859281ae0205dabe63c50b100406756a7ee
ZDT@MAC <dev@dthappy.com>
2016-05-14 10:10:43 +0800

    add avataar

--- chatApp/src/components/Message.jsx
+++ chatApp/src/components/Message.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {List, ListItem}  from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+
 
 class Message extends Component {
     constructor(props){
@@ -7,7 +9,9 @@ class Message extends Component {
     }
     render() {
         return (
-            <ListItem>
+            <ListItem
+                leftAvatar={<Avatar src="https://avatars0.githubusercontent.com/u/4001992?v=3&u=9f76a26691adb57dcf0b28015282dbd92f14c1d3&s=140"/>}
+            >
                 {this.props.message}
             </ListItem>
         );
