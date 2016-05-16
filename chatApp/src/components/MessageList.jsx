d4d0f13349fb6f64fd420fd3f5a548ae0decc097
ZDT@MAC <dev@dthappy.com>
2016-05-16 23:13:51 +0800

    use lodash to work with objects instead of arrays

--- chatApp/src/components/MessageList.jsx
+++ chatApp/src/components/MessageList.jsx
@@ -14,7 +14,15 @@ class MessageList extends Component {
         };
         this.firebaseRef = new Firebase('https://chatappio.firebaseio.com/messages');
         this.firebaseRef.once("value",(dataSnapshot)=>{
-            var messages = dataSnapshot.val();
+            var messagesVal = dataSnapshot.val();
+            var messages = _(messagesVal)
+            .keys()
+            .map((messageKey)=>{
+                var cloned = _.clone(messagesVal[messageKey]);
+                cloned.key = messageKey;
+                return cloned;
+            })
+            .value();
             this.setState({
                 messages:messages
             });
