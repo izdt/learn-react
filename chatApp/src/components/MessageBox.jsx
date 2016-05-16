181f819e0eb4211d3e058d7a543b3866adba4e2e
ZDT@MAC <dev@dthappy.com>
2016-05-16 23:31:30 +0800

    add event to textarea

--- chatApp/src/components/MessageBox.jsx
+++ chatApp/src/components/MessageBox.jsx
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
 import Card from 'material-ui/Card';
+import trim from 'trim';
 
 class MessageBox extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            message: ''
+        }
     }
+    
+    onChange(evt){
+        this.setState({
+            message: evt.target.value
+        });
+    }
+    
+    onKeyUp(evt){
+        if(evt.keyCode===13 && trim(evt.target.value) != ''){
+            evt.preventDefault();
+            this.setState({
+                message:''
+            });
+            console.log('Sent a new message: '+evt.target.value);
+        } 
+    }
+    
     render() {
         return (
             <Card style={{
@@ -12,7 +33,11 @@ class MessageBox extends Component {
                 margin: '30px auto',
                 padding: 10
             }}>
-                <textarea style={{
+                <textarea
+                value={this.state.message} 
+                onChange={this.onChange.bind(this)}
+                onKeyUp={this.onKeyUp.bind(this)} 
+                style={{
                     width: '100%',
                     borderColor: '#D0D0D0',
                     resize: 'none',
