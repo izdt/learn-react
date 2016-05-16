518dad139327cf7fd427c66d6e090f6c371ef844
ZDT@MAC <dev@dthappy.com>
2016-05-16 22:30:13 +0800

    add eslint and firebase data

--- chatApp/src/components/MessageList.jsx
+++ chatApp/src/components/MessageList.jsx
@@ -3,6 +3,7 @@ import Message from './Message.jsx';
 import Card from 'material-ui/Card';
 import List from 'material-ui/List';
 
+
 class MessageList extends Component {
     constructor(props){
         super(props);
