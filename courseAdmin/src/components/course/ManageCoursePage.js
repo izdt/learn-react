dac478fe501e7c4841e121b9edb65f9dfda9ddc6
ZDT@MAC <dev@dthappy.com>
2016-07-04 20:39:31 +0800

    bind event to update course

--- courseAdmin/src/components/course/ManageCoursePage.js
+++ courseAdmin/src/components/course/ManageCoursePage.js
@@ -11,7 +11,17 @@ class ManageCoursePage extends Component {
             course: Object.assign({},this.props.course),
             errors:{}
         };
+
+        this.updateCourseState = this.updateCourseState.bind(this);
     }
+
+    updateCourseState(event){
+        const field = event.target.name;
+        let course = this.state.course;
+        course[field] = event.target.value;
+        return this.setState({course:course});
+    }
+
     render() {
         return (
             <div>
@@ -19,6 +29,7 @@ class ManageCoursePage extends Component {
                 allAuthors={this.props.authors}
                 course={this.state.course} 
                 errors={this.state.errors}
+                onChange={this.updateCourseState}
                 />
             </div>
         );
