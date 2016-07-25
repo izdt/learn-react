336b9d08ae7911b87c3fd0cf3d255115d912198e
ZDT@VAIO <dev@dthappy.com>
2016-07-25 23:13:59 +0800

    test thunks with async action

--- courseAdmin/src/actions/courseActions.test.js
+++ courseAdmin/src/actions/courseActions.test.js
@@ -22,14 +22,34 @@ describe ('Course Action', () => {
     });
 });
 
-const midddleware = [thunk];
-const mockData = configureMockStore(midddleware);
 
-describe ('Asyn Actions', () => {
+
+const midddleware = [thunk];
+const mockStore = configureMockStore(midddleware);
+
+describe ('Async Actions', () => {
     afterEach(() => {
         nock.cleanAll();
     });
-    it ('Should CREATE_COURSE_SUCCESS and LOAD_COURSES_SUCCESS when loading courses', (done) => {
-        //TODO
+    it ('Should BEGIN_AJAX_CALL and LOAD_COURSES_SUCCESS when loading courses', (done) => {
+        //example to call nock
+        /*
+        nock('http://www.api.com')
+            .get('courses')
+            .reply(200,{body:{course:[{id:1,firstName:'HELLO',lastName:'ABC'}]}});
+        */
+        const expectedActions = [
+            {type:types.BEGIN_AJAX_CALL},
+            {type:types.LOAD_COURSES_SUCCESS, body: {courses: [{id:'clean-code',title:'Clean Code'}]}}
+        ];
+        
+        const store = mockStore({courses:[]}, expectedActions);
+        store.dispatch(courseActions.loadCourses()).then(()=>{
+            const actions = store.getActions();
+            expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL);
+            expect(actions[1].type).toEqual(types.LOAD_COURSES_SUCCESS);
+            done();
+        });
+
     });
 });
