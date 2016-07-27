7a0eb28cb4d9c481dbbc648b5feb514a3bae4824
ZDT@VAIO <dev@dthappy.com>
2016-07-27 22:39:19 +0800

    add build to production

---
+++ courseAdmin/tools/build.js
@@ -0,0 +1,31 @@
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.prod';
+import colors from 'colors';
+
+process.env.NODE_ENV = 'production';
+
+console.log('Generting minified bundle for production via Webpack');
+
+webpack(webpackConfig).run((err, stats) => {
+    if(err){
+        console.log(err.bold.red);
+        return 1;
+    }
+    
+    const jsonStats = stats.toJson();
+
+    if(jsonStats.hasErrors){
+        return jsonStats.errors.map(err0r => console.log(error.red));
+    }
+
+    if(jsonStats.hasWarnings){
+        console.log('Webpack generated the following warings: '.bold.yellow);
+        jsonStats.warnings.map(warning => console.log(warning.yellow));
+    }
+
+    console.log(`Webpack stats:${stats}`);
+
+    console.log('complied in production mode');
+
+    return 0;
+});
