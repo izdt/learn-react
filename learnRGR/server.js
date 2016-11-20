19269ae5424dbe2bd2810aff46c9ab9d94fffc51
ZDT@VAIO <dev@dthappy.com>
2016-11-20 15:20:54 +0800

    add webpack

--- learnRGR/server.js
+++ learnRGR/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 let app = express();
 
-app.get('/', (req,res)=>res.send('Hello express!'));
+app.use(express.static('public'));
+//app.get('/', (req,res)=>res.send('Hello express!'));
 
 app.listen(3000);
---
+++ learnRGR/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-2']
+        }
+      }
+    ]
+  }
+};
