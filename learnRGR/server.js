279e4458762eb9147f51d232ed002d3963b733d5
ZDT@VAIO <dev@dthappy.com>
2016-11-20 22:57:22 +0800

    add data api to express

--- learnRGR/server.js
+++ learnRGR/server.js
@@ -5,14 +5,18 @@ import MongoConfig from './db/mongodb.config'
 let app = express();
 
 app.use(express.static('public'));
-//app.get('/', (req,res)=>res.send('Hello express!'));
 
-app.listen(3000);
+let db;
 
 MongoClient.connect(MongoConfig.connectString, (err, database)=>{
     if(err) throw err;
-    database.collection('links').find({}).toArray((err,links)=>{
+    db = database;
+    app.listen(3000,()=>console.log('Listening on port 3000...'));
+});
+
+app.get('/data/links',(req,res)=>{
+    db.collection('links').find({}).toArray((err,links)=>{
         if(err) throw err;
-        console.log(links);
+        res.json(links);
     });
 });
