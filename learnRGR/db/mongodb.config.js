137c27ec4c946e765a1af3f0738e0a9f82bdd26a
ZDT@VAIO <dev@dthappy.com>
2016-11-20 22:14:57 +0800

    add mongodb

---
+++ learnRGR/db/mongodb.config.js
@@ -0,0 +1,4 @@
+let mongodbConfig = {
+    connectString:'mongodb://<dbuser>:<dbpassword>@ds050879.mlab.com:50879/idb'
+};
+export default mongodbConfig;
--- learnRGR/server.js
+++ learnRGR/server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import {MongoClient} from 'mongodb';
+import MongoConfig from 'db/mongodb.config'
 
 let app = express();
 
@@ -6,3 +8,11 @@ app.use(express.static('public'));
 //app.get('/', (req,res)=>res.send('Hello express!'));
 
 app.listen(3000);
+
+MongoClient.connect(MongoConfig.connectString, (err, database)=>{
+    if(err) throw err;
+    database.collection('links').find({}).toArray((err,links)=>{
+        if(err) throw err;
+        console.log(links);
+    });
+});
