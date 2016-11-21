f1bd51313b69438a458f3e67e78ffc568f1eb36d
ZDT@VAIO <dev@dthappy.com>
2016-11-21 19:13:07 +0800

    add main component

---
+++ learnRGR/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                Hello from React Hello
+            </div>
+        );
+    }
+}
+
+export default Main;
