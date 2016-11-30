bb0f1c7693080989674b28fe12e065b26accbb90
ZDT@VAIO <dev@dthappy.com>
2016-11-30 23:26:43 +0800

    use flux store to fetch data

--- learnRGR/src/components/Main.js
+++ learnRGR/src/components/Main.js
@@ -2,14 +2,38 @@ import React, {Component} from 'react';
 import API from '../API';
 import LinkStore from '../stores/LinkStore';
 
+let _getAppState = () => {
+    return { links: LinkStore.getAll() };
+}
+
 class Main extends Component {
+    constructor(prop){
+        super(prop);
+        this.state = _getAppState();
+        this.onChange = this.onChange.bind(this);
+    }
     componentWillMount(){
         API.fetchLinks();
+        LinkStore.on('change',this.onChange);
+    }
+    componentWillUnMount(){
+        LinkStore.removeListener('change',this.onChange);
+    }
+    onChange(){
+        this.setState(_getAppState());
     }
     render() {
+        let content = this.state.links.map(link=>{
+            return <li key={link._id}>
+                     <a href={link.url}>{link.title}</a>
+                   </li>
+        });
         return (
             <div>
-                Hello from React Main
+                <h3>Links:</h3>
+                <ul>
+                    {content}
+                </ul>
             </div>
         );
     }
