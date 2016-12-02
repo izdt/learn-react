efb32d6c7df54dbe4f4cb1cad30c093f0df99e76
ZDT@VAIO <dev@dthappy.com>
2016-12-02 22:26:54 +0800

    use GraphQLList counterType

--- learnRGR/data/schema.js
+++ learnRGR/data/schema.js
@@ -7,14 +7,32 @@ import {
     GraphQLString
 } from 'graphql';
 
-let data = [42,43,44];
+let data = [
+    { counter: 23 },
+    { counter: 24 },
+    { counter: 25 },    
+];
 
+let counterType = new GraphQLObjectType({
+    name: 'Counter',
+    fields: {
+        counter: {type: GraphQLInt}
+    }
+});
+
+/**
+ {
+  data {
+    counter
+  }
+ }
+ */
 let schema = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'Query',
         fields: {
             data:{
-                type: new GraphQLList(GraphQLInt),
+                type: new GraphQLList(counterType),
                 resolve: () => data
             }
         }
