ff3867236a9b88bc35ed0e8e27e3258bc05c6d25
ZDT@VAIO <dev@dthappy.com>
2016-12-02 22:07:50 +0800

    add query and mutation with GraphQL

--- learnRGR/data/schema.js
+++ learnRGR/data/schema.js
@@ -1,17 +1,33 @@
 import {
+    graphql,
     GraphQLSchema,
     GraphQLObjectType,
-    GraphQLint,
+    GraphQLInt,
     GraphQLString
 } from 'graphql';
+
+let counter = 42;
 
 let schema = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'Query',
         fields: {
             counter: {
-                type: GraphQLint,
-                resolve: () => 42
+                type: GraphQLInt,
+                resolve: () => counter
+            },
+            message: {
+                type: GraphQLString,
+                resolve: () => "Hello GraphQL"
+            }
+        }
+    }),
+    mutation: new GraphQLObjectType({
+        name: 'Mutation',
+        fields: {
+            incrementCounter: {
+                type: GraphQLInt,
+                resolve: () => ++counter
             }
         }
     })
