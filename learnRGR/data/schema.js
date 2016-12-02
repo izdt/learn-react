71a33c3cd64b251d7b3cc52204fc87c051bb9af0
ZDT@VAIO <dev@dthappy.com>
2016-12-02 22:19:05 +0800

    use GraphQLList(GraphQLInt)

--- learnRGR/data/schema.js
+++ learnRGR/data/schema.js
@@ -2,32 +2,20 @@ import {
     graphql,
     GraphQLSchema,
     GraphQLObjectType,
+    GraphQLList,
     GraphQLInt,
     GraphQLString
 } from 'graphql';
 
-let counter = 42;
+let data = [42,43,44];
 
 let schema = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'Query',
         fields: {
-            counter: {
-                type: GraphQLInt,
-                resolve: () => counter
-            },
-            message: {
-                type: GraphQLString,
-                resolve: () => "Hello GraphQL"
-            }
-        }
-    }),
-    mutation: new GraphQLObjectType({
-        name: 'Mutation',
-        fields: {
-            incrementCounter: {
-                type: GraphQLInt,
-                resolve: () => ++counter
+            data:{
+                type: new GraphQLList(GraphQLInt),
+                resolve: () => data
             }
         }
     })
