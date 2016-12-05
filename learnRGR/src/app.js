e474ce53e4e5ee3e577d77b40ab42db122c0fcd9
ZDT@VAIO <dev@dthappy.com>
2016-12-05 23:25:09 +0800

    install react-relay

--- learnRGR/src/app.js
+++ learnRGR/src/app.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Relay from 'react-relay';
 import Main from './components/Main';
 
 ReactDOM.render(<Main />,document.getElementById('app'));
 //export default Hello;
+
+console.log(
+    Relay.QL `
+    {
+        links {
+            titile
+        }
+    }
+    `
+);
